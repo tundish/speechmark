8802de0f29bb07c86b4781a418c071175a5558b7
tundish <tundish@gigeconomy.org.uk>
2023-03-17 18:23:58 +0000

    Runs on Python 3.10.

---
+++ speechmark/__init__.py
@@ -0,0 +1,13 @@
+import datetime
+import importlib.metadata
+
+try:
+    import tomllib
+except ImportError:
+    import tomli as tomllib
+
+
+try:
+    __version__ = importlib.metadata.version("nmskit")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = datetime.date.today().strftime("%Y.%m.%d") + "+local_repository"
--- speechmark/test/test_speechmark.py
+++ speechmark/test/test_speechmark.py
@@ -1,33 +1,35 @@
 #!/usr/bin/env python
 #   encoding: utf-8
 
-# This is part of the Balladeer library.
+# This is part of the speechmark library.
 # Copyright (C) 2023 D E Haynes
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
 
-# This program is distributed in the hope that it will be useful,
+# This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+# USA
 
 import html
 import re
 import textwrap
-import tomllib
 import unittest
 
-import balladeer
-from balladeer.lite.speechmark import SpeechMark
+import speechmark
+from speechmark.speechmark import SpeechMark
+from speechmark import tomllib
 
 __doc__ = f"""
-:Version: {balladeer.__version__}
+:Version: {speechmark.__version__}
 :Author: D E Haynes
 :Licence: `CC BY-NC-ND <https://creativecommons.org/licenses/by-nc-nd/4.0/>`_ Attribution-NonCommercial-NoDerivs
 
